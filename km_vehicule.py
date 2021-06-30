ef2a35af18f479919dc5c027ca23b2afe5844237
morty <laniakea.sgaetoile@gmx.fr>
2021-06-30 12:39:49 +0200

    Nouveau script de départ propre

--- km_vehicule.py
+++ km_vehicule.py
@@ -8,7 +8,7 @@ dico_km[veh1] = int(input("Entrez les km du véhicule_1 :\n"))
 
 dico_km[veh2] = int(input("Entrez les km du véhicule_2 :\n"))
 
-dico_km[veh3] = int(input("Entrez les km du véhicule_ 3 :\n"))
+dico_km[veh3] = int(input("Entrez les km du véhicule_3 :\n"))
 
 
 print(dico_km)
