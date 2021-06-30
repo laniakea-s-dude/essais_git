520b822297aa21ac055de4658ec353b9806d7c5a
morty <laniakea.sgaetoile@gmx.fr>
2021-06-30 12:08:42 +0200

    Nouveau script de départ

--- km_vehicule.py
+++ km_vehicule.py
@@ -1,14 +1,14 @@
 dico_km = {}
 
-veh1 = "vehicule 1"
-veh2 = "vehicule 2"
-veh3 = "vehicule 3"
+veh1 = "vehicule_1"
+veh2 = "vehicule_2"
+veh3 = "vehicule_3"
 
-dico_km[veh1] = int(input("Entrez les km du véhicule 1 :\n"))
+dico_km[veh1] = int(input("Entrez les km du véhicule_1 :\n"))
 
-dico_km[veh2] = int(input("Entrez les km du véhicule 2 :\n"))
+dico_km[veh2] = int(input("Entrez les km du véhicule_2 :\n"))
 
-dico_km[veh3] = int(input("Entrez les km du véhicule 3 :\n"))
+dico_km[veh3] = int(input("Entrez les km du véhicule_ 3 :\n"))
 
 
 print(dico_km)
