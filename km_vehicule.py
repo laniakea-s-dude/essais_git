6f700c3b258f9a00b9f463ddb7c363c3c3b8b96d
morty <laniakea.sgaetoile@gmx.fr>
2021-07-01 13:38:02 +0200

    Retouches noms des véhicules

--- km_vehicule.py
+++ km_vehicule.py
@@ -1,17 +1,17 @@
 dico_km = {}
 
-veh1 = "vehicule_1"
-veh2 = "vehicule_2"
-veh3 = "vehicule_3"
+veh1 = "vehicule n°1"
+veh2 = "vehicule n°2"
+veh3 = "vehicule n°3"
 
-dico_km[veh1] = int(input("Entrez les km du véhicule_1 :\n"))
+dico_km[veh1] = int(input("Entrez les km du véhicule n°1 :\n"))
 
-dico_km[veh2] = int(input("Entrez les km du véhicule_2 :\n"))
+dico_km[veh2] = int(input("Entrez les km du véhicule n°2 :\n"))
 
-dico_km[veh3] = int(input("Entrez les km du véhicule_3 :\n"))
+dico_km[veh3] = int(input("Entrez les km du véhicule n°3 :\n"))
 
 
 print(dico_km)
 
 for ve, k in dico_km.items():
-    print(ve, "est à", k, "km")
+    print("Le", ve, "est à", k, "km")
